20758087fbaff5fcacbea904b80081999d351517
DESKTOP-4KFFSLA\xl <xian9yu@gmail.com>
2022-11-03 14:54:08 +0800

    已上传postman接口参数

--- utils/pagination.go
+++ utils/pagination.go
@@ -2,14 +2,22 @@ package utils
 
 // Pagination 分页
 type Pagination struct {
-	PageNo    int `form:"page"`
-	LimitSize int `form:"size"`
+	PageNo   int `form:"page"` //页码
+	PageSize int `form:"size"` //每页显示数量
 }
 
 func (p Pagination) Offset() int {
-	offset := (p.PageNo - 1) * p.LimitSize
+	offset := (p.PageNo - 1) * p.PageSize
 	if offset < 0 {
 		offset = 0
 	}
 	return offset
+}
+
+// 每页默认显示数量 10
+func (p Pagination) DefaultPageSize(pageSize int) int {
+	if pageSize == 0 {
+		pageSize = 10
+	}
+	return pageSize
 }
